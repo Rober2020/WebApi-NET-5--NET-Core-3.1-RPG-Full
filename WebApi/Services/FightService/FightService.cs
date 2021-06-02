1f3bc663bb8b55f38c86adc1b20e2dd5930896e3
Rober2020 <62210597+Rober2020@users.noreply.github.com>
2021-06-02 09:20:15 -0400

    Update FightService.cs
    fin del combate Fight

--- WebApi/Services/FightService/FightService.cs
+++ WebApi/Services/FightService/FightService.cs
@@ -76,18 +76,14 @@ namespace WebApi.Services.FightService
                     .FirstOrDefaultAsync(x => x.Id == request.OpponentId);
 
                 var skill = attacker.Skills.FirstOrDefault(x => x.Id == request.SkillId);
-                if(skill == null)
+                if (skill == null)
                 {
                     response.Success = false;
                     response.Message = $"{attacker.Name} doesn't know this skill";
                     return response;
                 }
 
-                int damage = skill.Damage + (new Random().Next(attacker.Intelligence));
-                damage -= new Random().Next(opponent.Defense);
-
-                if (damage > 0)
-                    opponent.HitPoints -= damage;
+                int damage = DoSkillAttack(attacker, opponent, skill);
 
                 if (opponent.HitPoints <= 0)
                     response.Message = $"{opponent.Name} has been defeat";
@@ -109,6 +105,16 @@ namespace WebApi.Services.FightService
                 response.Message = ex.Message;
             }
             return response;
+        }
+
+        private static int DoSkillAttack(Character attacker, Character opponent, Skill skill)
+        {
+            int damage = skill.Damage + (new Random().Next(attacker.Intelligence));
+            damage -= new Random().Next(opponent.Defense);
+
+            if (damage > 0)
+                opponent.HitPoints -= damage;
+            return damage;
         }
 
         public async Task<ServiceResponse<FightResultDto>> Fight(FightRequestDto request)
@@ -142,8 +148,35 @@ namespace WebApi.Services.FightService
                             attackUsed = attacker.Weapon.Name;
                             damage = DoWeaponAttack(attacker, opponent);
                         }
+                        else
+                        {
+                            var skill = attacker.Skills[new Random().Next(attacker.Skills.Count)];
+                            attackUsed = skill.Name;
+                            damage = DoSkillAttack(attacker, opponent, skill);
+                        }
+
+                        response.Data.Log
+                            .Add($"{attacker.Name} attacks {opponent.Name} using {attackUsed} with {(damage >= 0 ? damage : 0)} damage.");
+
+                        if (opponent.HitPoints <= 0)
+                        {
+                            defeated = true;
+                            attacker.Victories++;
+                            opponent.Defeats++;
+                            response.Data.Log.Add($"{opponent.Name} has been defeated");
+                            response.Data.Log.Add($"{attacker.Name} win with {attacker.HitPoints} HP left!");
+                            break;
+                        }
                     }
                 }
+
+                characters.ForEach(x =>
+                {
+                    x.Fights++;
+                    x.HitPoints = 100;
+                });
+
+                await _context.SaveChangesAsync();
             }
             catch (Exception ex)
             {
